c5ff83617b2e8d484c87dcef1aa3cae8fe5de34d
ElleOwO <lmtdmya@gmail.com>
2024-09-15 00:07:02 -0600

    step 10: create a spawner

--- obstacle.gd
+++ obstacle.gd
@@ -1,11 +1,20 @@
 extends AnimatableBody2D
+class_name Obstacle
 
+var shrink_speed = 4.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$CollisionPolygon2D.scale = Vector2.ONE*10.0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _physics_process(delta: float) -> void:
+	$CollisionPolygon2D.scale -= Vector2.ONE * shrink_speed*delta
+
+# once tiny, delete
+	if $CollisionPolygon2D.scale.x < .05:
+		self.queue_free()
