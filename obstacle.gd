a3bff905290deec41f998d36000b8c904b727051
ElleOwO <lmtdmya@gmail.com>
2024-09-19 23:04:18 -0600

    step 12: Make it Fancy

--- obstacle.gd
+++ obstacle.gd
@@ -1,12 +1,12 @@
 extends AnimatableBody2D
 class_name Obstacle
-
+# changed shrink speed form 4 to 5
 var shrink_speed = 4.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$CollisionPolygon2D.scale = Vector2.ONE*10.0
-
+	$CollisionPolygon2D.rotate(randf_range(0,360))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
